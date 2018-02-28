c5ac2fb894fd21a3d0317ec70644c92810805a15
spoorthyv <sckortyman@gmail.com>
2018-02-28 11:43:31 -0800

    try 1

---
+++ src/main/java/myMavinTest/myMavinTest/App.java
@@ -0,0 +1,13 @@
+package myMavinTest.myMavinTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
